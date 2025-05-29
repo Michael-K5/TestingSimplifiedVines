fdffc8f00d808cd88fa8c94568447428b3f6204c
Michael Kraus <maik.kraus@aol.de>
2025-05-29 12:28:58 +0200

    adapted parameters and output

--- R/multiple_experiments.R
+++ R/multiple_experiments.R
@@ -9,18 +9,18 @@ nus <- list(10,4)
 # dimensions of the simulated data.
 dims <- list(3,4,5)
 # each list element contains first the lower, then the upper limit
-tau_limits <- list(c(0.001,0.6), c(0.001,0.9))
+tau_limits <- list(c(0.001,0.3), c(0.001,0.6), c(0.001,0.9))
 # Quantile Levels to calculate:
-lower_q_levs <- seq(0.01,0.25,0.01)
-upper_q_levs <- seq(0.75,0.99,0.01)
+lower_q_levs <- seq(0.01,0.30,0.01)
+upper_q_levs <- seq(0.70,0.99,0.01)
 
 # Define 3d parameters
 struct_mat_3d <- matrix(c(1,1,1,
                          2,2,0,
                          3,0,0), ncol=3, byrow=TRUE)
 families_3d <- list(list("frank", "gaussian"), list("frank"))
-params_3d <- list(c(ktau_to_par(family=families_3d[[1]][[2]], tau=-0.2)),
-                  c(ktau_to_par(family=families_3d[[1]][[2]], tau=0.3)))
+params_3d <- list(c(ktau_to_par(family=families_3d[[1]][[1]], tau=0.2)),
+                  c(ktau_to_par(family=families_3d[[1]][[2]], tau=0.4)))
 rotations_3d <- list(list(0,0),0)
 # Define 4d parameters
 struct_mat_4d <- matrix(c(3,3,3,3,
@@ -30,7 +30,7 @@ struct_mat_4d <- matrix(c(3,3,3,3,
 families_4d <- list(list("frank", "gaussian","gaussian"),
                    list("gaussian","frank"),
                    list("frank"))
-params_4d <- list(c(ktau_to_par(family=families_4d[[1]][[2]], tau=-0.2)),
+params_4d <- list(c(ktau_to_par(family=families_4d[[1]][[1]], tau=0.2)),
                  c(ktau_to_par(family=families_4d[[1]][[2]], tau=0.3)),
                  c(ktau_to_par(family=families_4d[[1]][[3]], tau=0.7)))
 rotations_4d <- list(list(0,0,0),list(0,0), list(0))
@@ -44,9 +44,9 @@ families_5d <- list(list("frank", "gaussian","gaussian","frank"),
                     list("frank","gaussian","gaussian"),
                     list("gaussian", "frank"),
                     list("gaussian"))
-params_5d <- list(c(ktau_to_par(family=families_5d[[1]][[1]], tau=-0.2)),
+params_5d <- list(c(ktau_to_par(family=families_5d[[1]][[1]], tau=0.2)),
                   c(ktau_to_par(family=families_5d[[1]][[2]], tau=0.3)),
-                  c(ktau_to_par(family=families_5d[[1]][[3]], tau=-0.1)),
+                  c(ktau_to_par(family=families_5d[[1]][[3]], tau=0.4)),
                   c(ktau_to_par(family=families_5d[[1]][[4]], tau=0.1)))
 rotations_5d <- list(list(0,0,0,0),list(0,0,0), list(0,0), list(0))
 # Summarize parameters by dimension
@@ -112,10 +112,10 @@ run_simulations <- function(num_samples,
           param_cond_func_3d_lin <- list(
             list(u_to_param_linear(c(1), tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_func_3d_non_lin <- list(
-            list(u_to_param_non_lin(c(1,1),
+            list(u_to_param_quadratic(c(1,1),
                                     tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_func_3d_non_lin_cubic <- list(
-            list(u_to_param_non_lin_cubic(c(1,1.2,0.5),
+            list(u_to_param_cubic(c(1,1.2,0.5),
                                           tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_func_var <- list(
             param_cond_func_3d_lin,
@@ -128,19 +128,19 @@ run_simulations <- function(num_samples,
             list(u_to_param_linear(c(0.4,0.6), tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_funcs_4d_non_lin <- list(
             list(
-              u_to_param_non_lin(c(1,1.4),
+              u_to_param_quadratic(c(1,1.4),
                                  tau_lower=tau_lower, tau_upper=tau_upper),
-              u_to_param_non_lin(c(0.6,0.8),
+              u_to_param_quadratic(c(1.2,0.9),
                                  tau_lower=tau_lower, tau_upper=tau_upper)),
             list(
-              u_to_param_non_lin(c(0.4,0.8, -0.3,0.7,1.3),
+              u_to_param_quadratic(c(-0.7,1.5,0.9,-1.2,1.5),
                                  tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_funcs_4d_non_lin_cubic <- list(
-            list(u_to_param_non_lin_cubic(c(1,0.5,0.7),
+            list(u_to_param_cubic(c(0.7,1.2,-0.8),
                                           tau_lower=tau_lower, tau_upper=tau_upper),
-                 u_to_param_non_lin_cubic(c(0.4,0.4,0.3),
+                 u_to_param_cubic(c(1.4,-1.8,1.0),
                                           tau_lower=tau_lower, tau_upper=tau_upper)),
-            list(u_to_param_non_lin_cubic(c(0.4,0.7, 0.2,-0.4,0.8,-0.9,0.6,0.2,0.4),
+            list(u_to_param_cubic(c(0.7,1.8, -0.9,1.4,0.8,1.1,-1.2,-1.3,1.7),
                                           tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_func_var <- list(
             param_cond_funcs_4d_lin,
@@ -156,33 +156,34 @@ run_simulations <- function(num_samples,
             list(u_to_param_linear(c(0.2,0.5,0.3), tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_funcs_5d_non_lin <- list(
             list(
-              u_to_param_non_lin(c(1,1),
+              u_to_param_quadratic(c(1,1),
                                  tau_lower=tau_lower, tau_upper=tau_upper),
-              u_to_param_non_lin(c(1,0.7),
+              u_to_param_quadratic(c(1,0.7),
                                  tau_lower=tau_lower, tau_upper=tau_upper),
-              u_to_param_non_lin(c(1,2),
+              u_to_param_quadratic(c(1,2),
                                  tau_lower=tau_lower, tau_upper=tau_upper)),
-            list(u_to_param_non_lin(c(0.7,0.3, 0.9,-0.4,0.8),
+            list(u_to_param_quadratic(c(0.7,0.5, 0.9,-1.2,1.5),
                                     tau_lower=tau_lower, tau_upper=tau_upper),
-                 u_to_param_non_lin(c(0.4,0.6,1,0.7,2),
+                 u_to_param_quadratic(c(0.4,-0.8,1.0,0.7,2.0),
                                     tau_lower=tau_lower, tau_upper=tau_upper)),
-            list(u_to_param_non_lin(c(0.2,0.5,0.3,1,2,1,0.4,0.7,0.8),
+            list(u_to_param_quadratic(c(0.7,0.4,-0.9,1.3,0.75,1.3,-0.6,0.5,-1.1),
                                     tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_funcs_5d_non_lin_cubic <- list(
-            list(u_to_param_non_lin_cubic(c(1,1,0.5),
+            list(u_to_param_cubic(c(1.0,1.0,0.5),
                                           tau_lower=tau_lower, tau_upper=tau_upper),
-                 u_to_param_non_lin_cubic(c(1,0.7,0.3),
+                 u_to_param_cubic(c(1.0,0.7,0.3),
                                           tau_lower=tau_lower, tau_upper=tau_upper),
-                 u_to_param_non_lin_cubic(c(1,2,0.1),
+                 u_to_param_cubic(c(1.0,2.0,1.1),
                                           tau_lower=tau_lower, tau_upper=tau_upper)),
-            list(u_to_param_non_lin_cubic(c(0.7,0.3, 0.9,-0.4,0.8,0.1,0.2,0.3,0.4),
+            list(u_to_param_cubic(c(0.7,0.8, -0.9,0.4,0.8,1.1,-1.2,-1.3,1.0),
                                           tau_lower=tau_lower, tau_upper=tau_upper),
-                 u_to_param_non_lin_cubic(c(0.4,0.6,1,0.7,1.8,0.01,0.04,0.07,0.03),
+                 u_to_param_cubic(c(0.4,0.6,1,0.7,2, -1.1,1.2,-0.9,-0.3),
                                           tau_lower=tau_lower, tau_upper=tau_upper)),
-            list(u_to_param_non_lin_cubic(c(0.2, 0.5, 0.3,
-                                            1,   1.4, 1  ,0.4,0.7,0.8,
-                                            0.01,0.04,0.07,0.03,0.02,0.05,0.07,0.04,0.02,0.1),
-                                          tau_lower=tau_lower, tau_upper=tau_upper)))
+            list(u_to_param_cubic(c(0.7, 0.5, -1.3,
+                                    1,   1.4, 1  ,-1.4,0.7,-0.8,
+                                    1.1,-1.4,0.7,-0.3,0.4,
+                                    0.8, -0.7,1.4,-1.2,-0.6),
+                                    tau_lower=tau_lower, tau_upper=tau_upper)))
           param_cond_func_var <- list(
             param_cond_funcs_5d_lin,
             param_cond_funcs_5d_non_lin,
@@ -198,7 +199,7 @@ run_simulations <- function(num_samples,
           print(paste0("Executing... ",
                        total_runs,
                        " of a total of ",
-                       print(length(dims)*length(num_samples)*length(tau_limits)*length(param_cond_func_var))))
+                       length(dims)*length(num_samples)*length(tau_limits)*length(param_cond_func_var)))
           non_simp_data <- simulate_non_simp_parallel(n_samples = num_samples[[sample_idx]],
                                                       struct = struct_mat_var,
                                                       families=families_var,
@@ -207,7 +208,7 @@ run_simulations <- function(num_samples,
                                                       rotations = rotations_var)
           orig_data <- as.matrix(non_simp_data)
           orig_data <- unname(orig_data)
-          fitted_vine <-vinecop(orig_data,family_set="onepar")
+          fitted_vine <-vinecop(orig_data,family_set="parametric")
           # number of samples to create from the fitted vine
           n_noise <- nu_var * nrow(orig_data)
           # simulate from the simplified vine, to train a classifier
@@ -220,7 +221,6 @@ run_simulations <- function(num_samples,
           x_test <- split_output[[2]]
           y_train <- split_output[[3]]
           y_test <- split_output[[4]]
-          #sum(y_train) /length(y_train)
           # delete model from previous iteration
           model <- 0
           # define new model
@@ -228,9 +228,11 @@ run_simulations <- function(num_samples,
           train_model_output <- train_model(model, x_train, y_train, num_epochs=200)
           model <- train_model_output[[1]]
           test_set_eval <- model %>% evaluate(x_test, y_test)
+          train_set_eval <- model %>% evaluate(x_train, y_train)
           g_vals <- compute_gvals(model, orig_data, nu=nu_var)
           output <- perform_quant_reg(g_vals,
                                       orig_data,
+                                      family_set_name="parametric",
                                       bottom_quantile_levels=lower_q_levs,
                                       top_quantile_levels = upper_q_levs)
           # Store the results of the current iteration
@@ -241,7 +243,11 @@ run_simulations <- function(num_samples,
                               par_idx,
                               output[[1]],
                               output[[2]],
-                              test_set_eval[[2]])
+                              train_set_eval[[1]],
+                              train_set_eval[[2]],
+                              test_set_eval[[1]],
+                              test_set_eval[[2]]
+                              )
           # append to the results list, as a list, to keep the rows separated
           all_results <- append(all_results, list(current_result))
 
@@ -262,6 +268,9 @@ run_simulations <- function(num_samples,
                             "param_cond_func_idx",
                             lower_quantile_names,
                             upper_quantile_names,
+                            "train set loss",
+                            "train set accuracy",
+                            "test set loss",
                             "test set accuracy")
   if(filename != ""){
     write.csv(results_df, file = filename, row.names = FALSE)
@@ -269,24 +278,42 @@ run_simulations <- function(num_samples,
   }
   return(results_df)
 }
-# # Run big simulation
-# # Get the current date in YYYY-MM-DD format
-# current_date <- Sys.Date()
-# # Construct the file name with the date
-# results_filename <- paste0("results/", current_date, ".csv")
-# result_df <- run_simulations(
-#   num_samples=num_samples,
-#   dims=dims,
-#   nus=nus,
-#   tau_limits=tau_limits,
-#   struct_mats=struct_mats,
-#   families=families,
-#   initial_params=initial_params,
-#   rotations=rotations,
-#   filename=results_filename
-#   )
-# head(results_df)
+# Run big simulation
+# Get the current date in YYYY-MM-DD format
+current_date <- Sys.Date()
+# Construct the file name with the date
+results_filename <- paste0("results/", current_date, ".csv")
+result_df <- run_simulations(
+  num_samples=num_samples,
+  dims=dims,
+  nus=nus,
+  tau_limits=tau_limits,
+  struct_mats=struct_mats,
+  families=families,
+  initial_params=initial_params,
+  rotations=rotations,
+  filename=results_filename
+  )
+head(result_df)
 
+# q_levs_lower_for_latex <- c(0.05,0.10,0.15,0.2)
+# q_levs_lower_latex_names <- paste0("q(", q_levs_lower_for_latex, ")>0")
+# q_levs_upper_for_latex <- c(0.95,0.9,0.85,0.8)
+# q_levs_upper_latex_names <- paste0("q(", q_levs_upper_for_latex, ")<0")
+# cols_for_latex <-c("num_samples",
+#                    "dim",
+#                    "tau_lower",
+#                    "tau_upper",
+#                    "param_cond_func_idx",
+#                    q_levs_lower_latex_names,
+#                    q_levs_upper_latex_names,
+#                    "train set loss",
+#                    "test set loss")
+# latex_result_df <- result_df[cols_for_latex]
+# head(latex_result_df)
+# latex_filename <- "results/LatexRelevantFields20250527.csv"
+# write.csv(latex_result_df, file = latex_filename, row.names = FALSE)
+# print(paste("Data saved to:", latex_filename))
 # Small test
 # result_df <- run_simulations(
 #   num_samples=list(1000),
@@ -338,4 +365,3 @@ run_simulations <- function(num_samples,
 # print(output[[1]] / nrow(orig_data))
 # plot(lower_q_levs, output[[1]])
 # plot(upper_q_levs, output[[2]])
-roxygen2::roxygenize()
